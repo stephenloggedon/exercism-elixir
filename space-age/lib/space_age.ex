babc969ba39bc391b0ab14c265fe4d7b2bce0409
stephen 💯 <sbrooks@STEPHEN1>
2023-10-28 13:43:48 -0700

    add space-age

---
+++ space-age/lib/space_age.ex
@@ -0,0 +1,32 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @earth_seconds 31557600
+  @conversion_chart %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  }
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet', or an error if 'planet' is not a planet.
+  """
+  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+  def age_on(planet, seconds) when is_map_key(@conversion_chart, planet), do:
+    {:ok, seconds / @earth_seconds / @conversion_chart[planet]}
+  def age_on(_, _), do: {:error, "not a planet"}
+end
